0abaa1440fa7b7bad993c8e96204e65b4f5f56e8
Vershilovicho <vernik281186@gmail.com>
2023-08-04 22:38:58 +0300

    Внесли изменения в задачи

--- Homework_01/Task_03.py
+++ Homework_01/Task_03.py
@@ -1,20 +1,32 @@
-#Программа загадывает число от 0 до 1000. Необходимо угадать число за 10 попыток. Программа должна подсказывать «больше» или «меньше» после каждой попытки. 
-#Для генерации случайного числа используйте код:
+# Программа загадывает число от 0 до 1000. Необходимо угадать число за 10 попыток. 
+# Программа должна подсказывать «больше» или «меньше» после каждой попытки. 
+# Для генерации случайного числа используйте код:
 
 from random import randint
-n = randint(1, 1000)
-k = int(input("Угадайте целое число от 1 до 1000. Попытка №1 - "))
-ATTEMPT = 2
-while (k != n) and (ATTEMPT <= 10):
-    k = int(input(f"Попытка № {ATTEMPT} (осталось попыток {10-ATTEMPT})  - "))  
-    if k < n:
-        print("Ваше число меньше, чем загадала программа")
-    elif k > n:
-        print("Ваше число больше, чем загадала программа")
+
+LOWER_LIMIT = 0
+UPPER_LIMIT = 1000
+
+num = randint(LOWER_LIMIT, UPPER_LIMIT) 
+print(num)
+print("Попробуйте угадать число: ")
+
+
+def guess_number(count):
+    attempts = 10
+    answer = int(input(f"\nПопытка № {count}. Введите число: "))
+
+    if count < attempts:
+        if answer > num:
+            print("\nМеньше! ")
+            return guess_number(count + 1)
+        elif answer < num:
+            print("\nБольше!")
+            return guess_number(count + 1)
+        elif answer == num:
+            print("\nВы угадали!")
     else:
-        print("Ура! Вы угадали!")
-       # print(f'Ваш ответ', k)
-       # print(f'Загадала программа', n)
-    #k = int(input(f'Повторите {p+1} попытку:')) 
-    ATTEMPT += 1  
-print ('Извините! Ваш лимит попыток ИСЧЕРПАН!')    
+        print("попытки закончились. Вы не угадали число...")
+
+count = 1
+guess_number(count)
